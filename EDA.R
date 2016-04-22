a0a5f19590f90a485a51b9e76c59c25aaf5cd079
Dirk Hesse <dirk.hesse@intelcom.no>
2016-04-22 16:28:17 +0200

    Initial commit.

---
+++ EDA.R
@@ -0,0 +1,23 @@
+library(readr)
+library(ggmap)
+library(dplyr)
+conflict.data <- read_csv("ACLED Version 6 All Africa 1997-2015_csv_dyadic.csv.gz")
+bbox <- make_bbox(LONGITUDE,
+                  LATITUDE,
+                  data=conflict.data,
+                  f=0.2)
+africa <- get_map(bbox)
+ggmap(africa) +
+    geom_point(aes(x=LONGITUDE,
+                   y=LATITUDE,
+                   color=YEAR,
+                   size=FATALITIES),
+               data=conflict.data %>%
+                   filter(FATALITIES < 5000),
+               alpha=0.5) +
+    xlim(-20, 40) +
+    ylim(-35, 35) +
+    scale_color_gradient(limits=c(1997, 2015),
+                         low="orangered1",
+                         high="red4")
+ggsave("africa_conflicts.png")
---
+++ README.md
@@ -0,0 +1,4 @@
+# Conflicts in Africa, Explored in R
+
+This is a collection of scripts connected to a post
+[on my blog](http://data-adventures.com).
